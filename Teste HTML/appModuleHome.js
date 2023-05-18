df99d4fdfb32e2b24d34745344707ebf815a012a
Marcus Escobar <marcusescobar10@gmail.com>
2023-05-18 11:45:13 -0300

    Alterações de segurança

--- Teste HTML/appModuleHome.js
+++ Teste HTML/appModuleHome.js
@@ -4,15 +4,7 @@ import { getStorage, ref, uploadBytes, getDownloadURL, listAll } from "https://w
 import { getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
 //firebse configs
-const firebaseConfig = {
-  apiKey: "AIzaSyDzRbVbrB7ZjJw-HylA5HM9DZ8Taf0eiiI",
-  authDomain: "woof0-75c1f.firebaseapp.com",
-  projectId: "woof0-75c1f",
-  storageBucket: "woof0-75c1f.appspot.com",
-  messagingSenderId: "10079225691",
-  appId: "1:10079225691:web:a8c0e222346ef945a4bc7e",
-  measurementId: "G-4R28M6JFQ4"
-};
+import firebaseConfig from './config'
 
 //////////////////////////////////////////HOME PAGE/////////////////////////////////////////////////////
 const app = initializeApp(firebaseConfig);
